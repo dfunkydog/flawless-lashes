ecea4da4c0394a3f85927e91469f09d346c41ee0
Jupiter <wengerball@gmail.com>
2014-02-27 16:12:38 +0000

    Added custom gallery type for homepage slider.
    Added social sharing buttons with each product.
    Added gallery type to make homepage gallery easier to manage.
    Added syling for block of text below home page gallery area
    Removed unneccasry styles

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
+BASELINE
 
+Created by 416 Studios.
 
-	BASELINE
+- - - - - - - - - - - - - - - - - - - - - - -
 
-	Created by 416 Studios.
+BASELINE custom wordpress theme for
+flawless lash shop.
 
-	- - - - - - - - - - - - - - - - - - - - - - -
+##NOTA BENE##
 
-	BASELINE is a bare-bones WordPress theme 
-	created to act as a starting point for the 
-	416 studios themes. 
+I have elected not to include css files in the
+repo.
+The code in .scss files are "prefix-free",
+Vendor prefixs are added after css compilation
+via "autoprefixer". How that is done is very
+much dependant on the dev environment.
 
-	
+Take a look and edit config.rb if necessary.
--- functions.php
+++ functions.php
@@ -210,3 +210,344 @@ add_filter( 'woocommerce_related_products_args', 'woo_related_products_limit' );
 
 remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart',30);
 add_Action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart',15);
+
+/***********add share buttons under add to cart ******/
+add_action('woocommerce_share','wooshare');
+function wooshare(){
+	echo'<div id="fl-woo-share">
+	<div class="fb-like" data-href="'.get_permalink().'" data-layout="button" data-send="false" data-width="100"></div>
+	<a href="https://twitter.com/share" class="twitter-share-button" data-count="none">Tweet</a>
+
+	<script>
+	    ! function (d, s, id) {
+	        var js, fjs = d.getElementsByTagName(s)[0];
+	        if (!d.getElementById(id)) {
+	            js = d.createElement(s);
+	            js.id = id;
+	            js.src = "//platform.twitter.com/widgets.js";
+	            fjs.parentNode.insertBefore(js, fjs);
+	        }
+	    }(document, "script", "twitter-wjs");
+	</script>
+	<a href="http://pinterest.com/pin/create/button/?url='. urlencode(get_permalink()).'&media='.urlencode(wp_get_attachment_url( get_post_thumbnail_id() )).'&description='.apply_filters( 'woocommerce_short_description', $post->post_excerpt ).'" class="pin-it-button"
+	count-layout="horizontal">
+	    <img border="0" src="//assets.pinterest.com/images/PinExt.png" title="Pin It" />
+	</a>
+	<script type="text/javascript" src="//assets.pinterest.com/js/pinit.js"></script>';?>
+
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=281787978603249";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+</div>
+	<?php
+}
+
+/***********Alternative gallery*************/
+remove_shortcode('gallery', 'gallery_shortcode');
+add_shortcode( 'gallery','fl_gallery' );
+function flw_gallery($attr){
+
+	$post = get_post();
+
+	static $instance = 0;
+	$instance++;
+
+	if ( ! empty( $attr['ids'] ) ) {
+		// 'ids' is explicitly ordered, unless you specify otherwise.
+		if ( empty( $attr['orderby'] ) )
+			$attr['orderby'] = 'post__in';
+		$attr['include'] = $attr['ids'];
+	}
+
+	// Allow plugins/themes to override the default gallery template.
+	$output = apply_filters('post_gallery', '', $attr);
+	if ( $output != '' )
+		return $output;
+
+	// We're trusting author input, so let's at least make sure it looks like a valid orderby statement
+	if ( isset( $attr['orderby'] ) ) {
+		$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+		if ( !$attr['orderby'] )
+			unset( $attr['orderby'] );
+	}
+
+	extract(shortcode_atts(array(
+		'order'      => 'ASC',
+		'orderby'    => 'menu_order ID',
+		'id'         => $post ? $post->ID : 0,
+		'itemtag'    => '',
+		'icontag'    => '',
+		'captiontag' => '',
+		'columns'    => 3,
+		'size'       => 'fullsize',
+		'include'    => '',
+		'exclude'    => '',
+		'link'       => ''
+	), $attr, 'gallery'));
+
+
+	$id = intval($id);
+	if ( 'RAND' == $order )
+		$orderby = 'none';
+
+	if ( !empty($include) ) {
+		$_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+
+		$attachments = array();
+		foreach ( $_attachments as $key => $val ) {
+			$attachments[$val->ID] = $_attachments[$key];
+		}
+	} elseif ( !empty($exclude) ) {
+		$attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	} else {
+		$attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	}
+
+	if ( empty($attachments) )
+		return '';
+
+	if ( is_feed() ) {
+		$output = "\n";
+		foreach ( $attachments as $att_id => $attachment )
+			$output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+		return $output;
+	}
+
+	$itemtag = tag_escape($itemtag);
+	$captiontag = tag_escape($captiontag);
+	$icontag = tag_escape($icontag);
+	$valid_tags = wp_kses_allowed_html( 'post' );
+	if ( ! isset( $valid_tags[ $itemtag ] ) )
+		$itemtag = '';
+	if ( ! isset( $valid_tags[ $captiontag ] ) )
+		$captiontag = '';
+	if ( ! isset( $valid_tags[ $icontag ] ) )
+		$icontag = '';
+
+	$columns = intval($columns);
+	$itemwidth = $columns > 0 ? floor(100/$columns) : 100;
+	$float = is_rtl() ? 'right' : 'left';
+
+	//$selector = "gallery-{$instance}";
+	$selector = "maximage";
+
+	$gallery_style = $gallery_div = '';
+	if ( apply_filters( 'use_default_gallery_style', true ) )
+		$gallery_style = "";
+	$size_class = sanitize_html_class( $size );
+	$gallery_div = "<div id='$selector' class='mc-cycle gallery galleryid-{$id} gallery-columns-{$columns} gallery-size-{$size_class}'>";
+	$output = apply_filters( 'gallery_style', $gallery_style . "\n\t\t" . $gallery_div );
+
+	$i = 0;
+	foreach ( $attachments as $id => $attachment ) {
+		if ( ! empty( $link ) && 'file' === $link )
+			$image_output = wp_get_attachment_link( $id, $size, false, false );
+		elseif ( ! empty( $link ) && 'none' === $link )
+			$image_output = wp_get_attachment_image( $id, $size, false, array( 'class' => 'mc-image','title'=>$post->post_title) );
+		else
+			$image_output = wp_get_attachment_link( $id, $size, true, false );
+
+		$image_meta  = wp_get_attachment_metadata( $id );
+
+		$orientation = '';
+		if ( isset( $image_meta['height'], $image_meta['width'] ) )
+			$orientation = ( $image_meta['height'] > $image_meta['width'] ) ? 'portrait' : 'landscape';
+
+
+		$output .= $image_output;
+		if ( $captiontag && trim($attachment->post_excerpt) ) {
+			$output .= wptexturize($attachment->post_excerpt);
+		}
+
+	}
+
+	$output .= "
+		</div>\n";
+
+
+	return $output;
+
+		}
+
+		/*****END OF CUSTOM GALLERY ****/
+
+		/**
+ * The Gallery shortcode.
+ *
+ * This implements the functionality of the Gallery Shortcode for displaying
+ * WordPress images on a post.
+ *
+ * @since 2.5.0
+ *
+ * @param array $attr Attributes of the shortcode.
+ * @return string HTML content to display gallery.
+ */
+function fl_gallery($attr) {
+	$post = get_post();
+
+	static $instance = 0;
+	$instance++;
+
+	if ( ! empty( $attr['ids'] ) ) {
+		// 'ids' is explicitly ordered, unless you specify otherwise.
+		if ( empty( $attr['orderby'] ) )
+			$attr['orderby'] = 'post__in';
+		$attr['include'] = $attr['ids'];
+	}
+
+	// Allow plugins/themes to override the default gallery template.
+	$output = apply_filters('post_gallery', '', $attr);
+	if ( $output != '' )
+		return $output;
+
+	// We're trusting author input, so let's at least make sure it looks like a valid orderby statement
+	if ( isset( $attr['orderby'] ) ) {
+		$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+		if ( !$attr['orderby'] )
+			unset( $attr['orderby'] );
+	}
+
+	extract(shortcode_atts(array(
+		'order'      => 'ASC',
+		'orderby'    => 'menu_order ID',
+		'id'         => $post ? $post->ID : 0,
+		'itemtag'    => 'dl',
+		'icontag'    => 'dt',
+		'captiontag' => 'dd',
+		'columns'    => 3,
+		'size'       => 'thumbnail',
+		'include'    => '',
+		'exclude'    => '',
+		'link'       => '',
+		'type'       => 'default_style',
+	), $attr, 'gallery'));
+	if($type != 'default_style') $size = 'large';
+
+	$id = intval($id);
+	if ( 'RAND' == $order )
+		$orderby = 'none';
+
+	if ( !empty($include) ) {
+		$_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+
+		$attachments = array();
+		foreach ( $_attachments as $key => $val ) {
+			$attachments[$val->ID] = $_attachments[$key];
+		}
+	} elseif ( !empty($exclude) ) {
+		$attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	} else {
+		$attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	}
+
+	if ( empty($attachments) )
+		return '';
+
+	if ( is_feed() ) {
+		$output = "\n";
+		foreach ( $attachments as $att_id => $attachment )
+			$output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+		return $output;
+	}
+
+	$itemtag = tag_escape($itemtag);
+	$captiontag = tag_escape($captiontag);
+	$icontag = tag_escape($icontag);
+	$valid_tags = wp_kses_allowed_html( 'post' );
+	if ( ! isset( $valid_tags[ $itemtag ] ) )
+		$itemtag = 'dl';
+	if ( ! isset( $valid_tags[ $captiontag ] ) )
+		$captiontag = 'dd';
+	if ( ! isset( $valid_tags[ $icontag ] ) )
+		$icontag = 'dt';
+
+	$columns = intval($columns);
+	$itemwidth = $columns > 0 ? floor(100/$columns) : 100;
+	$float = is_rtl() ? 'right' : 'left';
+
+	$selector = "gallery-{$instance}";
+
+	$gallery_style = $gallery_div = '';
+	if ( apply_filters( 'use_default_gallery_style', true ) )
+		$gallery_style = "
+		<style type='text/css'>
+			#{$selector} {
+				margin: auto;
+			}
+			#{$selector} .gallery-item {
+				float: {$float};
+				margin-top: 10px;
+				text-align: center;
+				width: {$itemwidth}%;
+			}
+			#{$selector} img {
+				border: 2px solid #cfcfcf;
+			}
+			#{$selector} .gallery-caption {
+				margin-left: 0;
+			}
+			/* see gallery_shortcode() in wp-includes/media.php */
+		</style>";
+		$size_class = sanitize_html_class( $size );
+	if($type == 'default_style') {
+		$gallery_div = "<div id='$selector' class='gallery {$type} galleryid-{$id} gallery-columns-{$columns} gallery-size-{$size_class}'>";
+		$output = apply_filters( 'gallery_style', $gallery_style . "\n\t\t" . $gallery_div );
+	} else {
+		$output = "<div id='$selector' class='fl-gallery'>\n";
+		$output .="<div class='cycle-slideshow' data-cycle-fx='scrollHorz' data-cycle-pause-on-hover='true' data-cycle-prev='#prev' data-cycle-next='#next'>\n";
+	}
+
+
+	$i = 0;
+	if($type == 'default_style'){
+		foreach ( $attachments as $id => $attachment ) {
+			if ( ! empty( $link ) && 'file' === $link )
+				$image_output = wp_get_attachment_link( $id, $size, false, false );
+			elseif ( ! empty( $link ) && 'none' === $link )
+				$image_output = wp_get_attachment_image( $id, $size, false );
+			else
+				$image_output = wp_get_attachment_link( $id, $size, true, false );
+
+			$image_meta  = wp_get_attachment_metadata( $id );
+
+			$orientation = '';
+			if ( isset( $image_meta['height'], $image_meta['width'] ) )
+				$orientation = ( $image_meta['height'] > $image_meta['width'] ) ? 'portrait' : 'landscape';
+
+			$output .= "<{$itemtag} class='gallery-item'>";
+			$output .= "
+				<{$icontag} class='gallery-icon {$orientation}'>
+					$image_output
+				</{$icontag}>";
+			if ( $captiontag && trim($attachment->post_excerpt) ) {
+				$output .= "
+					<{$captiontag} class='wp-caption-text gallery-caption'>
+					" . wptexturize($attachment->post_excerpt) . "
+					</{$captiontag}>";
+			}
+			$output .= "</{$itemtag}>";
+			if ( $columns > 0 && ++$i % $columns == 0 )
+				$output .= '<br style="clear: both" />';
+		}
+	} else {
+		foreach ($attachments as $id => $attachment) {
+			$image_output = wp_get_attachment_image( $id, $size, false );
+			$output .= $image_output;
+		}
+	}
+
+	if($type == 'default_style'){$output .= "
+			<br style='clear: both;' />
+		</div>\n";} else {
+			$output .= "</div>\n
+			<div id='slider-nav'><a id='prev' href='#''>Prev</a><a id='next' href='#'>Next</a></div>
+			</div>";
+		}
+
+	return $output;
+}
